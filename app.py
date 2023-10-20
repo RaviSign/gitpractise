e5355f97cfb6164a9656d98a0876b279dadc8aa4
RaviSign <ravis.ra@outlook.com>
2023-10-20 17:25:18 +0530

    Update correction in appp.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 def addition(a,b):
     return a+b
-<<<<<<< HEAD
 def division(a,b):
     return a/b
-=======
-def Multiple(a,b):
+def multiple(a,b):
     return a*b
->>>>>>> f0a7e30b1efafd0d701ec402ecaeaaa610a00051
